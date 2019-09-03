8e3a71041891da437d246358a80f2ce53daf3830
ramondeallima <ramondeallima@gmail.com>
2019-09-03 12:48:22 -0300

    Adicionado contador de palavras e caracteres

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,13 @@ var numPalavras = frase.split(' ').length
 var tamanhoFrase = $('#tamanho-frase')
 
 tamanhoFrase.text(numPalavras)
+
+var campo = $('.campo-digitacao')
+campo.on('input', function () {
+    var conteudo = campo.val()
+    var qtdPalavras = conteudo.split(/\S+/).length - 1
+    var qtdCaracteres = conteudo.length
+
+    $('#contador-caracteres').text(qtdCaracteres)    
+    $('#contador-palavras').text(qtdPalavras)
+})
