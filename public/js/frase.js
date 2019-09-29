3bc5a6ce4f835a0ee16672e843bb21f8668ab55a
Ramon Almeida <ramondeallima@gmail.com>
2019-09-28 23:46:16 -0300

    Função de buscar frase por id implementada

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $('#botao-frase').click(fraseAleatoria)
+$('#botao-frase-id').click(BuscaFrase)
 
 function fraseAleatoria(){
     $('#spinner').show()
@@ -21,4 +22,27 @@ function TrocaFraseAleatoria(data) {
 
     AtualizaTamanhoFrase()
     AtualizaTempoInicial(data[numAleatorio].tempo)
+}
+
+function BuscaFrase() {
+    $('#spinner').show()
+    var fraseId = $('#frase-id').val()
+    var dados = {id: fraseId}
+    $.get('http://localhost:3000/frases', dados, TrocaFrase)
+        .fail(function () {
+            $('#erro').show()
+            setTimeout(function () {
+                $('#erro').fadeOut(2000)
+            },2000)
+        })
+        .always(function () {
+            $('#spinner').toggle()
+        })
+}
+
+function TrocaFrase(data) {
+    var frase = $('.frase')
+    frase.text(data.texto)
+    AtualizaTempoInicial(data.tempo)
+    AtualizaTamanhoFrase()
 }
